4b31b6cfb3a0cf6bc48df0fcd257d8ec249b6c24
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2022-12-05 09:28:22 +0000

    Update pirateapp.py
    I think that this might work on streamlit now?

--- pirateapp.py
+++ pirateapp.py
@@ -1,15 +1,17 @@
+import streamlit as st
+
 # Initialize the game
-print("Welcome to the Pirate Adventure game!")
-print("You are a pirate on a quest to find treasure on a ship.")
+st.title("Pirate Adventure Game 🏴‍☠️ 🦜 ⚔️")
+st.header("You are a pirate on a quest to find treasure on a ship. 🚢 ⚓ 💰 ")
 
 # Initialize the rooms on the ship
 rooms = {
   "galley": (0, 0),
-  "lower deck": (0, 1),
-  "upperdeck": (0, 2),
-  "captain's quarters": (1, 0),
-  "crew's quarters": (1, 1),
-  "treasure room": (1, 2)
+  "lower_deck": (0, 1),
+  "upper_deck": (0, 2),
+  "captain's_quarters": (1, 0),
+  "crew's_quarters": (1, 1),
+  "treasure_room": (1, 2)
 }
 
 # Initialize variables for the game
@@ -19,20 +21,20 @@ player_room = "galley"
 # Main game loop
 while not treasure_found:
   # Print the current room of the player
-  print("You are currently in the", player_room)
+  st.write("You are currently in the", player_room)
 
   # Ask the player which room they want to move to
-  room = input("Which room do you want to move to? (galley, lower deck, upper deck, captain's quarters, crew's quarters) ")
+  room = st.selectbox("Which room do you want to move to?", ["galley", "lower_deck", "upper_deck", "captain's_quarters", "crew's_quarters"])
 
   # Update the player's current room
   if room in rooms:
     player_room = room
   else:
-    print("That room does not exist on the ship.")
+    st.write("That room does not exist on the ship.")
 
   # Check if the player has found the treasure
   if player_room == "treasure_room":
     treasure_found = True
 
 # The player has found the treasure!
-print("Congratulations, you have found the treasure!")
+st.write("Congratulations, you have found the treasure!")
