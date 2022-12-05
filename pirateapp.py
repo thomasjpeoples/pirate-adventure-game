81f61c764b0396a31e7f249ca9122ae3887eee0e
thomasjpeoples <55585074+thomasjpeoples@users.noreply.github.com>
2022-12-05 10:51:50 +0000

    Update pirateapp.py

--- pirateapp.py
+++ pirateapp.py
@@ -54,7 +54,7 @@ if not state.game_over:
     while not treasure_found:
         
       # Ask the player which room they want to move to
-      room = st.text_input("Which room do you want to move to?", key=state.game_number, , placeholder="galley")
+      room = st.text_input("Which room do you want to move to?", key=state.game_number, , placeholder=galley)
         
       # Update the player's current room
       if room in rooms:
